ad6365831e06ca96fac348f256a3557f1d6dc6a3
Lee <MATTHEW_LEE2@homedepot.com>
2016-07-12 14:25:49 -0500

    Added graph problems

---
+++ GraphProblems/Dijkstra/Vertex.java
@@ -0,0 +1,20 @@
+
+public class Vertex {
+	final private String id;
+	final private String name;
+	
+	public Vertex(String id, String name) {
+		this.id = id;
+		this.name = name;
+	}
+	
+	public String getId() {
+		return id;
+	}
+	
+	public String getName() {
+		return name;
+	}
+	
+	
+}
